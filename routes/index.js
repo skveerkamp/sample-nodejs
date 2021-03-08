c020da6d15ad7c36cce80aa7cc1826ef81d44b8e
Shannon Veerkamp <skveerkamp@gmail.com>
2021-03-08 16:46:04 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,9 @@ const LoremIpsum = require('lorem-ipsum').LoremIpsum;
 const lorem = new LoremIpsum({
   sentencesPerParagraph: {
     max: 8,
-    min: 4
   },
   wordsPerSentence: {
     max: 16,
-    min: 4
   }
 });
 
